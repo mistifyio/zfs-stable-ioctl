openzfs/zfs

Implement lzc_destroy_one()
This was originally intended to be `lzc_destroy()`, but recent versions
of the Linux kernel appear to have removed the information we need to
unmount datasets from the kernel driver without resorting to symbol
table hacks. Consequently, unmounting requires doing upcalls to userland
and error handling becomes easier if we delegate unmounting to the
userland caller.

Signed-off-by: Richard Yao <ryao@gentoo.org>

lib/libzfs/libzfs_dataset.c
@@ -3238,7 +3238,8 @@ zfs_create(libzfs_handle_t *hdl, const char *path, zfs_type_t type,
 int
 zfs_destroy(zfs_handle_t *zhp, boolean_t defer)
 {
-	zfs_cmd_t zc = {"\0"};
+	nvlist_t *opts = NULL;
+	int ret = 0;
 
 	if (zhp->zfs_type == ZFS_TYPE_BOOKMARK) {
 		nvlist_t *nv = fnvlist_alloc();
@@ -3253,17 +3254,16 @@ zfs_destroy(zfs_handle_t *zhp, boolean_t defer)
 		return (0);
 	}
 
-	(void) strlcpy(zc.zc_name, zhp->zfs_name, sizeof (zc.zc_name));
-
-	if (ZFS_IS_VOLUME(zhp)) {
-		zc.zc_objset_type = DMU_OST_ZVOL;
-	} else {
-		zc.zc_objset_type = DMU_OST_ZFS;
+	if (defer) {
+		opts = fnvlist_alloc();
+		fnvlist_add_boolean(opts, "defer_destroy");
 	}
 
-	zc.zc_defer_destroy = defer;
-	if (zfs_ioctl(zhp->zfs_hdl, ZFS_IOC_DESTROY, &zc) != 0 &&
-	    errno != ENOENT) {
+	ret = lzc_destroy_one(zhp->zfs_name, opts);
+	if (defer)
+		fnvlist_free(opts);
+
+	if (ret != 0 && errno != ENOENT) {
 		return (zfs_standard_error_fmt(zhp->zfs_hdl, errno,
 		    dgettext(TEXT_DOMAIN, "cannot destroy '%s'"),
 		    zhp->zfs_name));

lib/libzfs/libzfs_sendrecv.c
@@ -1853,7 +1853,7 @@ static int
 recv_destroy(libzfs_handle_t *hdl, const char *name, int baselen,
     char *newname, recvflags_t *flags)
 {
-	zfs_cmd_t zc = {"\0"};
+	nvlist_t *opts = NULL;
 	int err = 0;
 	prop_changelist_t *clp;
 	zfs_handle_t *zhp;
@@ -1876,17 +1876,22 @@ recv_destroy(libzfs_handle_t *hdl, const char *name, int baselen,
 	if (err)
 		return (err);
 
-	zc.zc_objset_type = DMU_OST_ZFS;
-	zc.zc_defer_destroy = defer;
-	(void) strlcpy(zc.zc_name, name, sizeof (zc.zc_name));
+	if (defer) {
+		opts = fnvlist_alloc();
+		fnvlist_add_boolean(opts, "defer_destroy");
+	}
 
 	if (flags->verbose)
-		(void) printf("attempting destroy %s\n", zc.zc_name);
-	err = ioctl(hdl->libzfs_fd, ZFS_IOC_DESTROY, &zc);
+		(void) printf("attempting destroy %s\n", name);
+	err = lzc_destroy_one(name, opts);
+
+	if (defer)
+		fnvlist_free(opts);
+
 	if (err == 0) {
 		if (flags->verbose)
 			(void) printf("success\n");
-		changelist_remove(clp, zc.zc_name);
+		changelist_remove(clp, name);
 	}
 
 	(void) changelist_postfix(clp);

module/zfs/zfs_ioctl.c
@@ -3810,6 +3810,39 @@ zfs_ioc_destroy_bookmarks(const char *poolname, nvlist_t *innvl,
 	return (error);
 }
 
+
+/* If ostype is DMU_OST_NONE, we perform a lookup. */
+static int
+zfs_destroy_impl(const char *fsname, dmu_objset_type_t ostype,
+	boolean_t defer_destroy)
+{
+	int err;
+
+	if (ostype == DMU_OST_NONE) {
+		objset_t *os;
+
+		err = dmu_objset_hold(fsname, FTAG, &os);
+		if (err != 0)
+			return (err);
+		ostype = dmu_objset_type(os);
+		dmu_objset_rele(os, FTAG);
+	}
+
+	if (ostype == DMU_OST_ZFS) {
+		err = zfs_unmount_snap(fsname);
+		if (err != 0)
+			return (err);
+	}
+
+	if (strchr(fsname, '@'))
+		err = dsl_destroy_snapshot(fsname, defer_destroy);
+	else
+		err = dsl_destroy_head(fsname);
+	if (ostype == DMU_OST_ZVOL && err == 0)
+		(void) zvol_remove_minor(fsname);
+	return (err);
+}
+
 /*
  * inputs:
  * zc_name		name of dataset to destroy
@@ -3821,21 +3854,8 @@ zfs_ioc_destroy_bookmarks(const char *poolname, nvlist_t *innvl,
 static int
 zfs_ioc_destroy(zfs_cmd_t *zc)
 {
-	int err;
-
-	if (zc->zc_objset_type == DMU_OST_ZFS) {
-		err = zfs_unmount_snap(zc->zc_name);
-		if (err != 0)
-			return (err);
-	}
-
-	if (strchr(zc->zc_name, '@'))
-		err = dsl_destroy_snapshot(zc->zc_name, zc->zc_defer_destroy);
-	else
-		err = dsl_destroy_head(zc->zc_name);
-	if (zc->zc_objset_type == DMU_OST_ZVOL && err == 0)
-		(void) zvol_remove_minor(zc->zc_name);
-	return (err);
+	return (zfs_destroy_impl(zc->zc_name, zc->zc_objset_type,
+	    zc->zc_defer_destroy));
 }
 
 /*
@@ -6088,6 +6108,14 @@ zfs_stable_ioc_zfs_list(const char *fsname, nvlist_t *innvl,
 	return (error);
 }
 
+static int
+zfs_stable_ioc_zfs_destroy(const char *fsname, nvlist_t *innvl,
+    nvlist_t *outnvl, nvlist_t *opts, uint64_t version)
+{
+	return (zfs_destroy_impl(fsname, DMU_OST_NONE,
+	    nvlist_exists(opts, "defer_destroy")));
+}
+
 /*
  * ioctl table for stable interface.
  * Functions use zfs_/zpool_ prefixes to distinguish between their uses
@@ -6249,6 +6277,14 @@ static const zfs_stable_ioc_vec_t zfs_stable_ioc_vec[] = {
 {	.zvec_name		= "zfs_inherit",
 	.zvec_func		= zfs_stable_ioc_zfs_inherit,
 	.zvec_secpolicy		= zfs_secpolicy_inherit_prop,
+	.zvec_namecheck		= DATASET_NAME,
+	.zvec_pool_check	= POOL_CHECK_SUSPENDED | POOL_CHECK_READONLY,
+	.zvec_smush_outnvlist	= B_FALSE,
+	.zvec_allow_log		= B_TRUE,
+},
+{	.zvec_name		= "zfs_destroy",
+	.zvec_func		= zfs_stable_ioc_zfs_destroy,
+	.zvec_secpolicy		= zfs_secpolicy_destroy,
 	.zvec_namecheck		= DATASET_NAME,
 	.zvec_pool_check	= POOL_CHECK_SUSPENDED | POOL_CHECK_READONLY,
 	.zvec_smush_outnvlist	= B_FALSE,

mistifyio/zfs-stable-ioctl

lib/libzfs/libzfs_dataset.c
@@ -3284,7 +3284,8 @@ zfs_create(libzfs_handle_t *hdl, const char *path, zfs_type_t type,
 int
 zfs_destroy(zfs_handle_t *zhp, boolean_t defer)
 {
-	zfs_cmd_t zc = {"\0"};
+	nvlist_t *opts = NULL;
+	int ret = 0;
 
 	if (zhp->zfs_type == ZFS_TYPE_BOOKMARK) {
 		nvlist_t *nv = fnvlist_alloc();
@@ -3299,17 +3300,16 @@ zfs_destroy(zfs_handle_t *zhp, boolean_t defer)
 		return (0);
 	}
 
-	(void) strlcpy(zc.zc_name, zhp->zfs_name, sizeof (zc.zc_name));
-
-	if (ZFS_IS_VOLUME(zhp)) {
-		zc.zc_objset_type = DMU_OST_ZVOL;
-	} else {
-		zc.zc_objset_type = DMU_OST_ZFS;
+	if (defer) {
+		opts = fnvlist_alloc();
+		fnvlist_add_boolean(opts, "defer");
 	}
 
-	zc.zc_defer_destroy = defer;
-	if (zfs_ioctl(zhp->zfs_hdl, ZFS_IOC_DESTROY, &zc) != 0 &&
-	    errno != ENOENT) {
+	ret = lzc_destroy_one(zhp->zfs_name, opts);
+	if (defer)
+		fnvlist_free(opts);
+
+	if (ret != 0 && errno != ENOENT) {
 		return (zfs_standard_error_fmt(zhp->zfs_hdl, errno,
 		    dgettext(TEXT_DOMAIN, "cannot destroy '%s'"),
 		    zhp->zfs_name));

lib/libzfs/libzfs_sendrecv.c
@@ -1862,7 +1862,7 @@ static int
 recv_destroy(libzfs_handle_t *hdl, const char *name, int baselen,
     char *newname, recvflags_t *flags)
 {
-	zfs_cmd_t zc = {"\0"};
+	nvlist_t *opts = NULL;
 	int err = 0;
 	prop_changelist_t *clp;
 	zfs_handle_t *zhp;
@@ -1885,17 +1885,22 @@ recv_destroy(libzfs_handle_t *hdl, const char *name, int baselen,
 	if (err)
 		return (err);
 
-	zc.zc_objset_type = DMU_OST_ZFS;
-	zc.zc_defer_destroy = defer;
-	(void) strlcpy(zc.zc_name, name, sizeof (zc.zc_name));
+	if (defer) {
+		opts = fnvlist_alloc();
+		fnvlist_add_boolean(opts, "defer");
+	}
 
 	if (flags->verbose)
-		(void) printf("attempting destroy %s\n", zc.zc_name);
-	err = ioctl(hdl->libzfs_fd, ZFS_IOC_DESTROY, &zc);
+		(void) printf("attempting destroy %s\n", name);
+	err = lzc_destroy_one(name, opts);
+
+	if (defer)
+		fnvlist_free(opts);
+
 	if (err == 0) {
 		if (flags->verbose)
 			(void) printf("success\n");
-		changelist_remove(clp, zc.zc_name);
+		changelist_remove(clp, name);
 	}
 
 	(void) changelist_postfix(clp);

module/zfs/zfs_ioctl.c
@@ -3784,6 +3784,39 @@ zfs_ioc_destroy_bookmarks(const char *poolname, nvlist_t *innvl,
 	return (error);
 }
 
+
+/* If ostype is DMU_OST_NONE, we perform a lookup. */
+static int
+zfs_destroy_impl(const char *fsname, dmu_objset_type_t ostype,
+	boolean_t defer_destroy)
+{
+	int err;
+
+	if (ostype == DMU_OST_NONE) {
+		objset_t *os;
+
+		err = dmu_objset_hold(fsname, FTAG, &os);
+		if (err != 0)
+			return (err);
+		ostype = dmu_objset_type(os);
+		dmu_objset_rele(os, FTAG);
+	}
+
+	if (ostype == DMU_OST_ZFS) {
+		err = zfs_unmount_snap(fsname);
+		if (err != 0)
+			return (err);
+	}
+
+	if (strchr(fsname, '@'))
+		err = dsl_destroy_snapshot(fsname, defer_destroy);
+	else
+		err = dsl_destroy_head(fsname);
+	if (ostype == DMU_OST_ZVOL && err == 0)
+		(void) zvol_remove_minor(fsname);
+	return (err);
+}
+
 /*
  * inputs:
  * zc_name		name of dataset to destroy
@@ -3795,21 +3828,8 @@ zfs_ioc_destroy_bookmarks(const char *poolname, nvlist_t *innvl,
 static int
 zfs_ioc_destroy(zfs_cmd_t *zc)
 {
-	int err;
-
-	if (zc->zc_objset_type == DMU_OST_ZFS) {
-		err = zfs_unmount_snap(zc->zc_name);
-		if (err != 0)
-			return (err);
-	}
-
-	if (strchr(zc->zc_name, '@'))
-		err = dsl_destroy_snapshot(zc->zc_name, zc->zc_defer_destroy);
-	else
-		err = dsl_destroy_head(zc->zc_name);
-	if (zc->zc_objset_type == DMU_OST_ZVOL && err == 0)
-		(void) zvol_remove_minor(zc->zc_name);
-	return (err);
+	return (zfs_destroy_impl(zc->zc_name, zc->zc_objset_type,
+	    zc->zc_defer_destroy));
 }
 
 /*
@@ -6154,6 +6174,14 @@ zfs_stable_ioc_zfs_list(const char *fsname, nvlist_t *innvl,
 	return (error);
 }
 
+static int
+zfs_stable_ioc_zfs_destroy(const char *fsname, nvlist_t *innvl,
+    nvlist_t *outnvl, nvlist_t *opts, uint64_t version)
+{
+	return (zfs_destroy_impl(fsname, DMU_OST_NONE,
+	    nvlist_exists(opts, "defer")));
+}
+
 /*
  * ioctl table for stable interface.
  * Functions use zfs_/zpool_ prefixes to distinguish between their uses
@@ -6315,6 +6343,14 @@ static const zfs_stable_ioc_vec_t zfs_stable_ioc_vec[] = {
 {	.zvec_name		= "zfs_inherit",
 	.zvec_func		= zfs_stable_ioc_zfs_inherit,
 	.zvec_secpolicy		= zfs_secpolicy_inherit_prop,
+	.zvec_namecheck		= DATASET_NAME,
+	.zvec_pool_check	= POOL_CHECK_SUSPENDED | POOL_CHECK_READONLY,
+	.zvec_smush_outnvlist	= B_FALSE,
+	.zvec_allow_log		= B_TRUE,
+},
+{	.zvec_name		= "zfs_destroy",
+	.zvec_func		= zfs_stable_ioc_zfs_destroy,
+	.zvec_secpolicy		= zfs_secpolicy_destroy,
 	.zvec_namecheck		= DATASET_NAME,
 	.zvec_pool_check	= POOL_CHECK_SUSPENDED | POOL_CHECK_READONLY,
 	.zvec_smush_outnvlist	= B_FALSE,