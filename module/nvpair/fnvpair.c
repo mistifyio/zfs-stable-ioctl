92d9de97e2dfa73bcfc1be2c3a1c4c462ba3290a
Manuel Mendez <mmendez@omniti.com>
2015-10-27 13:25:14 -0400

    nvpair: expand fnvpair_pack encoding options
    Most fnvpair_pack is refactored to fnvpair_pack_enc which adds an encoding
    argument. fnvpair_pack is kept for backwards compatibility.
    
    Signed-off-by: Manuel Mendez <mmendez@omniti.com>

--- module/nvpair/fnvpair.c
+++ module/nvpair/fnvpair.c
@@ -74,12 +74,42 @@ fnvlist_size(nvlist_t *nvl)
  * fnvlist_pack_free().
  */
 char *
-fnvlist_pack(nvlist_t *nvl, size_t *sizep)
+fnvlist_pack_enc(nvlist_t *nvl, size_t *sizep, int encoding)
 {
 	char *packed = 0;
-	VERIFY3U(nvlist_pack(nvl, &packed, sizep, NV_ENCODE_NATIVE,
+	VERIFY3U(nvlist_pack(nvl, &packed, sizep, encoding,
 	    KM_SLEEP), ==, 0);
 	return (packed);
+}
+
+/*
+ * Returns allocated buffer of size *sizep.  Caller must free the buffer with
+ * fnvlist_pack_free(). An alias for fnvlist_pacn_native.
+ */
+char *
+fnvlist_pack(nvlist_t *nvl, size_t *sizep)
+{
+	return fnvlist_pack_native(nvl, sizep);
+}
+
+/*
+ * Returns allocated buffer of size *sizep.  Caller must free the buffer with
+ * fnvlist_pack_free(). Allocated buffer is native encoded.
+ */
+char *
+fnvlist_pack_native(nvlist_t *nvl, size_t *sizep)
+{
+	return fnvlist_pack_enc(nvl, sizep, NV_ENCODE_NATIVE);
+}
+
+/*
+ * Returns allocated buffer of size *sizep.  Caller must free the buffer with
+ * fnvlist_pack_free(). Allocated buffer is XDR encoded.
+ */
+char *
+fnvlist_pack_xdr(nvlist_t *nvl, size_t *sizep)
+{
+	return fnvlist_pack_enc(nvl, sizep, NV_ENCODE_XDR);
 }
 
 /*ARGSUSED*/
@@ -514,6 +544,7 @@ EXPORT_SYMBOL(fnvlist_alloc);
 EXPORT_SYMBOL(fnvlist_free);
 EXPORT_SYMBOL(fnvlist_size);
 EXPORT_SYMBOL(fnvlist_pack);
+EXPORT_SYMBOL(fnvlist_pack_xdr);
 EXPORT_SYMBOL(fnvlist_pack_free);
 EXPORT_SYMBOL(fnvlist_unpack);
 EXPORT_SYMBOL(fnvlist_dup);
